0e39604dda182ca46b525d2fb663af802ad97d40
欢 <2711452309@qq.com>
2025-02-14 20:45:50 +0800

    Update tcp.sh

--- app/BBR/tcp.sh
+++ app/BBR/tcp.sh
@@ -8,7 +8,7 @@ export PATH
 #=================================================
 
 sh_ver="1.4.0"
-github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
+github="raw.githubusercontent.com/huan-starvm/Linux-NetSpeed/master"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
