a1acf8792a39dd07bac50def27e2bd1b45af6ae7
欢 <2711452309@qq.com>
2025-02-18 04:41:19 +0800

    Update Debian-Ubuntu.sh

--- app/LXC-Shell/Debian-Ubuntu.sh
+++ app/LXC-Shell/Debian-Ubuntu.sh
@@ -4,25 +4,26 @@
 echo "此脚本由 星空云(https://www.starvm.cn) 编写，免费开源，如为收费均为倒卖，请勿相信"
 
 # 提示用户是否执行脚本
-read -p "是否开始执行脚本？(Y/y 是，n 否): " user_input
+read -rp "是否开始执行脚本？(Y/y 是，n 否): " user_input
 
 # 判断用户输入
 if [[ "$user_input" =~ ^[Yy]$ ]]; then
-  # 输出提示信息
-  echo "正在运行中，请勿断开SSH..."
+    echo "正在运行中，请勿断开SSH..."
 
-  # 更新包列表并升级所有软件包（在后台运行）
-  nohup bash -c "
+    # 更新包列表并升级所有软件包
     apt update && apt full-upgrade -y
-    && apt install -y vim sudo net-tools wget curl bash
-    && sed -i '/^#PermitRootLogin prohibit-password/ s/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-    && echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
-    && systemctl restart ssh
-    && echo '运行完成!'
-  " &
 
-  # 继续输出运行提示
-  echo "脚本已在后台运行，运行完成后会显示提示"
+    # 安装必需的软件包
+    apt install -y vim sudo net-tools wget curl bash
+
+    # 修改 SSH 配置以允许 root 登录
+    sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sed -i 's/^#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+    # 重启 SSH 服务
+    systemctl restart ssh
+
+    echo "执行完成"
 else
-  echo "取消执行"
+    echo "取消执行"
 fi
