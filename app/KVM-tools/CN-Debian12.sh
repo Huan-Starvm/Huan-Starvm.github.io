ee0a27716948d42cf29062462b264917f2b92005
欢 <2711452309@qq.com>
2025-04-24 02:27:36 +0800

    Update CN-Debian12.sh

--- app/KVM-tools/CN-Debian12.sh
+++ app/KVM-tools/CN-Debian12.sh
@@ -18,6 +18,9 @@ deb-src http://mirrors.tuna.tsinghua.edu.cn/debian-security bookworm-security ma
 EOF
 
 # 更新软件包索引以及系统环境
+export DEBIAN_FRONTEND=noninteractive
+echo "openssh-server ssh/permit-root-login select No" | sudo debconf-set-selections
+echo "openssh-server ssh/password-authentication boolean true" | sudo debconf-set-selections
 apt-get update && apt-get full-upgrade -y
 
 # 清理无用软件包
